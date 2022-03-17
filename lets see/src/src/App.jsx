ad30c4af870eac81c4cd2c610834c11910c582b0
Madhusudan Sharma <msharma0077865555@gmail.com>
2022-03-17 16:34:30 +0530

    new

--- lets see/src/src/App.jsx
+++ lets see/src/src/App.jsx
@@ -1,45 +1,71 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [books, setBooks] = useState(13);
+  const [pens, setPens] = useState(10);
+  const [notebooks, setNotebooks] = useState(44);
+  const [inkpens, setInkpens] = useState(78);
+
+  const handleBooks = (value) => {
+    setBooks(books + value);
+  };
+
+  const handlePens = (value) => {
+    setPens(pens + value);
+  };
+  const handleNotebooks = (value) => {
+    setNotebooks(notebooks + value);
+  };
+  const handleInkpens = (value) => {
+    setInkpens(inkpens + value);
+  };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <div className="items">
+        <span>Books:</span>
+        <button className="addBook" onClick={() => handleBooks(1)}>
+          +
+        </button>
+        <button className="remBook" onClick={() => handleBooks(-1)}>
+          -
+        </button>
+        <span className="totalBooks">{books}</span>
+      </div>
+      <div className="items">
+        <span>Pens:</span>
+        <button className="addPen" onClick={() => handlePens(1)}>
+          +
+        </button>
+        <button className="remPen" onClick={() => handlePens(-1)}>
+          -
+        </button>
+        <span className="totalPens">{pens}</span>
+      </div>
+      <div className="items">
+        <span>Notebooks:</span>
+        <button className="addNotebook" onClick={() => handleNotebooks(1)}>
+          +
+        </button>
+        <button className="remNotebook" onClick={() => handleNotebooks(-1)}>
+          -
+        </button>
+        <span className="totalNotebooks">{notebooks}</span>
+      </div>
+      <div className="items">
+        <span>Inkpens:</span>
+        <button className="addInkpen" onClick={() => handleInkpens(1)}>
+          +
+        </button>
+        <button className="remInkpen" onClick={() => handleInkpens(-1)}>
+          -
+        </button>
+        <span className="totalInkpens">{inkpens}</span>
+      </div>
+      <div className="total">{books + pens + notebooks + inkpens}</div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
