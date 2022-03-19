77fa7e0a57e44e70de48fcebe53caf16bcca52e3
Madhusudan Sharma <msharma0077865555@gmail.com>
2022-03-19 22:38:32 +0530

    new

--- day-3/src/src/App.jsx
+++ day-3/src/src/App.jsx
@@ -1,15 +1,34 @@
 import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
+    const getData = (data)=>{
+      console.log(data)
+      setCount(data)
+    }
+
   return (
     <div className="App">
-      
+      <First data={getData} />
+      <Right data = {count} />     
     </div>
   )
 }
-
+function First({data}){
+  const counter = 10
+    data(counter)
+  return <div>
+    Left:- {counter}
+  </div>
+}
+function Right({data}){
+   return <div>
+     Right:{data}
+     </div>
+}
 export default App
+
+// passing data from child to parent 
+// passing data from sibling to sibling
